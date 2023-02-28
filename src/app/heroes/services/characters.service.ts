2adec3787528dcbb4708541f684d314ffc167108
Juan Pablo Sánchez Rey <juanpablosanchezrey22@gmail.com>
2023-02-28 16:26:41 -0500

    a new method to search by id has been added

--- src/app/heroes/services/characters.service.ts
+++ src/app/heroes/services/characters.service.ts
@@ -2,16 +2,27 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Character } from '../interface/characters.interfaces';
+import { environment } from '../../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CharactersService {
 
+  private BaseUrl:string = environment.BaseUrl
+
   constructor( private http:HttpClient) { }
 
   getCharacters(): Observable<Character[]>{
-    return this.http.get<Character[]>('http://localhost:3000/characters')
+    return this.http.get<Character[]>(`${this.BaseUrl}/characters`)
+  }
+
+  getCharacterById(id: string): Observable<Character>{
+    return this.http.get<Character>(`${this.BaseUrl}/characters/${id}`)
+  }
+
+  getSuggestions(term: string): Observable<Character[]>{
+    return this.http.get<Character[]>(`${this.BaseUrl}/characters?q=${term}&_limit=5`)
   }
 
 }
